2b01e35de5d31e4bb72f23446645a9f0f3467f22
Mango Jelly Pudding <evan940415@gmail.com>
2022-10-27 13:20:11 +0800

    feat: init

--- src/re-directed/script.py
+++ src/re-directed/script.py
@@ -5,10 +5,10 @@ import re
 with open('./data.toml', 'rb') as file:
     data = tomllib.load(file)
 
-url_base = lambda url: data['url_base'].format(url=url)
+html_base = lambda url: data['html_base'].format(url=url)
 table_base = lambda domain, namespace, raw_url, transfer_url: \
     data['table_base'].format(domain=domain, namespace=namespace, raw_url=raw_url, transfer_url=transfer_url)
-transfer_url_base = lambda domain, filename: data['transfer_url_base'].format(domain=domain, filename=filename)
+transfer_url_base = lambda domain, filename: data['transfer_url_base'].format(url_base=data['url_base'], domain=domain, filename=filename)
 
 with open('./index.md', 'w', encoding='utf8') as index_file:
     index_file.write(data['markdown_base'])
@@ -22,7 +22,7 @@ for d_k, d_v in data.items():
         filename = re.match(r'(?:www)?\.(.+)\.', v_k).group(1)
         url = f'https://{v_k}{v_v}'
         with open(f'./{d_k}/{filename}.html', 'w', encoding='utf8') as file:
-            file.write(url_base(url))
+            file.write(html_base(url))
         with open('./index.md', 'a', encoding='utf8') as file:
             file.write(table_base(d_k, v_k+v_v, url, transfer_url_base(d_k, filename)))
         
