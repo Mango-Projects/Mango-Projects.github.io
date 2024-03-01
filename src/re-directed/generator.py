605c238849cd165c8dc3c273393b846a0d75a85c
Mango Jelly Pudding <evan940415@gmail.com>
2024-03-01 10:21:12 +0800

    remake redirected

---
+++ src/re-directed/generator.py
@@ -0,0 +1,58 @@
+import tomllib
+from pathlib import Path
+import re
+
+import pretty_errors
+
+import json
+
+
+def pprint(obj):
+    print(json.dumps(obj, ensure_ascii=False, indent=2))
+
+
+CURRENT_PATH = Path(__file__).parent
+
+with open(CURRENT_PATH / "data.toml", "rb") as file:
+    _data = tomllib.load(file)
+
+    TRANSFER_URL_BASE = _data["transfer_url_base"]
+    HTML_BASE = _data["html_base"]
+    MARKDOWN_BASE = _data["markdown_base"]
+    TABLE_BASE = _data["table_base"]
+    URL_MAPPING = _data["url_mapping"]
+
+with open(CURRENT_PATH / "url.toml", "rb") as file:
+    URL = tomllib.load(file)
+
+
+def generate_markdown():
+    lines = []
+    for namespace, data in URL.items():
+        for url_type, path in data.items():
+            lines.append(
+                TABLE_BASE.format(
+                    **{
+                        "namespace": namespace,
+                        "path": path,
+                        "original_url": URL_MAPPING[url_type] + path,
+                        "transfer_url": TRANSFER_URL_BASE.format(
+                            **{"namespace": namespace, "type": url_type}
+                        ),
+                    }
+                )
+            )
+    with open(CURRENT_PATH / "index.md", "w", encoding="utf8") as file:
+        file.write(MARKDOWN_BASE + "\n".join(lines))
+
+
+def generate_html(): ...
+
+
+def main():
+    generate_html()
+    generate_markdown()
+
+
+if __name__ == "__main__":
+    main()
